453a14464e269cb0b8ee69214529468213f23e69
Haneem <kmhaneem00@gmail.com>
2023-03-02 15:02:46 +0530

    Everything is done

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,12 @@
-console.log("Hello from backend");
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+app.get("/hello", (req, res) => {
+  return res.json({ message: "Hello" });
+});
+
+app.listen(3000, () => {
+  console.log("App is listening successfully");
+});
